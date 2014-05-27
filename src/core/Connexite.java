91013cb05bbbe96098f40a7a40eb025576a93887
realsoc <hugo.djemaa@gmail.com>
2014-05-27 10:53:38 +0200

    connexite retour chemin

--- src/core/Connexite.java
+++ src/core/Connexite.java
@@ -6,11 +6,11 @@ import base.Readarg ;
 public class Connexite extends Algo {
 
     public Connexite(Graphe gr, PrintStream sortie, Readarg readarg) {
-	super(gr, sortie, readarg) ;
+    	super(gr, sortie, readarg) ;
     }
 
     public Chemin run() {
-	// A vous d'implementer la recherche de connexite.
+    	return new Chemin();
     }
 
 }
